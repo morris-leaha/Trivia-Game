b0ebb0642284febfa1e633400adee00ce74b0be7
LAMorris <leahmorris@Leahs-MacBook-Pro.local>
2019-01-08 16:43:20 -0600

    added .show() and .hide() jQuery for instructions/questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,17 @@
 window.onload = function() {
-    $("#start").on("click", start);
+    $("#questions").hide();
+    $("#start").on("click",start);
+};
+
+var intervalId;
+var number = (120 * 1000);
+var clockRunning = false;
+
+function start() {
+    $("#questions").show();
+    $("#instructions").hide();
+    if (!clockRunning) {
+        intervalId = setInterval(decrement, 1000);
+        clockRunning = true;
+    }
 }
