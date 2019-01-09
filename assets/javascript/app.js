39753fcfddea5a260a86e2b93a740d049c63596b
LAMorris <leahmorris@dhcp-10-101-133-147.wireless.northwestern.private>
2019-01-08 20:23:19 -0600

    added functionality to show user results when time = 0

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,14 +6,27 @@ window.onload = function() {
 };
 
 var intervalId;
-var number = (120 * 1000);
+var number = 10;
 var clockRunning = false;
 
 function start() {
     $("#questions").show();
     $("#instructions").hide();
-    if (!clockRunning) {
-        intervalId = setInterval(decrement, 1000);
-        clockRunning = true;
+    intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+    number--;
+    $("#time-display").html(number);
+
+    if (number === 0) {
+        stop();
     }
+}
+
+function stop() {
+    clearInterval(intervalId);
+    $("#user-report-notsubmitted").show();
+    $("#questions").hide();
+    $("#instructions").hide();
 }
