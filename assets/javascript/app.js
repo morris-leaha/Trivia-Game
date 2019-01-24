c5c1d847825f4399025c958f3678b52fbb6c1a66
LAMorris <morris.leaha@gmail.com>
2019-01-24 15:36:04 -0600

    still working on checkAnswer() and changed background image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+window.onload = function () {
     $("#questions").hide();
     $(".timer").html("Time Remaining: " + " 02:00");
     $("#user-report").hide();
-    $("#start").on("click",start);
-    $("#submit").on("click",report);
+    $("#start").on("click", start);
+    $("#submit").on("click", report);
     $("#reset").on("click", reset);
 };
 
@@ -50,7 +50,7 @@ function report() {
     // checkAnswer();
     $("#questions").hide();
     $("#user-report").show();
-    
+
     if (correct === 10) {
         $("#msg").html("Perfect Score -- You must be a wine connoisseur!");
     } else if (correct < 10 && correct >= 6) {
@@ -62,254 +62,251 @@ function report() {
     }
 }
 
-    var correct = 0;
-    var incorrect = 0;
-    var unanswered = 0;
+var correct = 0;
+var incorrect = 0;
+var unanswered = 10;
 
-// function checkAnswer(q, a){
-//     var userAnswer = $("input[name=question" + q + "Radios]:checked", "#q-" + q).val(); 
-//         console.log("user answer= " + userAnswer);
-//     var q = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-//     var a = ["option1a", 
-//              "option2d", 
-//              "option3c", 
-//              "option4d", 
-//              "option5b", 
-//              "option6c", 
-//              "option7a", 
-//              "option8c", 
-//              "option9d", 
-//              "option10b"];
+function checkAnswer() {
+    var a = ["option1a",
+        "option2d",
+        "option3c",
+        "option4d",
+        "option5b",
+        "option6c",
+        "option7a",
+        "option8c",
+        "option9d",
+        "option10b"];
 
-//     for (var i = 0; i < a.length; i++) {
-//         if (userAnswer === a) {
-//             correct++;
-//             console.log("# correct " + correct); 
-//             $("#correct").html(correct);
-//             $("#incorrect").html(incorrect);
-//             unanswered--;
-//             $("#unanswered").html(unanswered);
-//         } else {
-//             incorrect++;
-//             console.log("# incorrect " + incorrect);
-//             $("#correct").html(correct);
-//             $("#incorrect").html(incorrect);
-//             unanswered--;
-//             $("#unanswered").html(unanswered);
-//             }
-//         }
-//     }
+    $("input[type=radio]:checked").each(function () {
+        console.log($(this).attr("id"));
 
-    $('#q-1 input').on('change', function() {
-        var q1 = $('input[name=question1Radios]:checked', '#q-1').val(); 
-        console.log("q1 value= " + q1);
-
-        if (q1 === "option1a") {
+        if (a.indexOf($(this).attr("id")) > -1) {
             correct++;
-            console.log("# correct " + correct); 
             $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
         } else {
             incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
             $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
         }
- });
+    });
 
-    $('#q-2 input').on('change', function() {
-        var q2 = $('input[name=question2Radios]:checked', '#q-2').val(); 
-        console.log("q2 value= " + q2);
+    unanswered -= correct + incorrect;
+    console.log(correct, incorrect, unanswered);
+    $("#unanswered").html(unanswered);
+}
 
-        if (q2 === "option2d") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
-    
- });
 
-    $('#q-3 input').on('change', function() {
-        var q3 = $('input[name=question3Radios]:checked', '#q-3').val(); 
-        console.log("q3 value= " + q3);
 
-        if (q3 === "option3c") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+$('#q-1 input').on('change', function () {
+    var q1 = $('input[name=question1Radios]:checked', '#q-1').val();
+    console.log("q1 value= " + q1);
+
+    if (q1 === "option1a") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
+});
+
+$('#q-2 input').on('change', function () {
+    var q2 = $('input[name=question2Radios]:checked', '#q-2').val();
+    console.log("q2 value= " + q2);
+
+    if (q2 === "option2d") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
 
 });
 
-    $('#q-4 input').on('change', function() {
-        var q4 = $('input[name=question4Radios]:checked', '#q-4').val(); 
-        console.log("q4 value= " + q4);
+$('#q-3 input').on('change', function () {
+    var q3 = $('input[name=question3Radios]:checked', '#q-3').val();
+    console.log("q3 value= " + q3);
 
-        if (q4 === "option4d") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+    if (q3 === "option3c") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
+
 });
 
-    $('#q-5 input').on('change', function() {
-        var q5 = $('input[name=question5Radios]:checked', '#q-5').val(); 
-        console.log("q5 value= " + q5);
+$('#q-4 input').on('change', function () {
+    var q4 = $('input[name=question4Radios]:checked', '#q-4').val();
+    console.log("q4 value= " + q4);
 
-        if (q5 === "option5b") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
- });
-
-    $('#q-6 input').on('change', function() {
-        var q6 = $('input[name=question6Radios]:checked', '#q-6').val(); 
-        console.log("q6 value= " + q6);
-
-        if (q6 === "option6c") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+    if (q4 === "option4d") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
 });
 
-    $('#q-7 input').on('change', function() {
-        var q7 = $('input[name=question7Radios]:checked', '#q-7').val(); 
-        console.log("q7 value= " + q7);
+$('#q-5 input').on('change', function () {
+    var q5 = $('input[name=question5Radios]:checked', '#q-5').val();
+    console.log("q5 value= " + q5);
 
-        if (q7 === "option7a") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+    if (q5 === "option5b") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
 });
 
-    $('#q-8 input').on('change', function() {
-        var q8 = $('input[name=question8Radios]:checked', '#q-8').val(); 
-        console.log("q8 value= " + q8);
+$('#q-6 input').on('change', function () {
+    var q6 = $('input[name=question6Radios]:checked', '#q-6').val();
+    console.log("q6 value= " + q6);
 
-        if (q8 === "option8c") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+    if (q6 === "option6c") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
 });
 
-    $('#q-9 input').on('change', function() {
-        var q9 = $('input[name=question9Radios]:checked', '#q-9').val(); 
-        console.log("q9 value= " + q9);
+$('#q-7 input').on('change', function () {
+    var q7 = $('input[name=question7Radios]:checked', '#q-7').val();
+    console.log("q7 value= " + q7);
 
-        if (q9 === "option9d") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+    if (q7 === "option7a") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
 });
 
-    $('#q-10 input').on('change', function() {
-        var q10 = $('input[name=question10Radios]:checked', '#q-10').val(); 
-        console.log("q10 value= " + q10);
+$('#q-8 input').on('change', function () {
+    var q8 = $('input[name=question8Radios]:checked', '#q-8').val();
+    console.log("q8 value= " + q8);
 
-        if (q10 === "option10b") {
-            correct++;
-            console.log("# correct " + correct); 
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        } else {
-            incorrect++;
-            console.log("# incorrect " + incorrect);
-            $("#correct").html(correct);
-            $("#incorrect").html(incorrect);
-            unanswered--;
-            $("#unanswered").html(unanswered);
-        }
+    if (q8 === "option8c") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
+});
+
+$('#q-9 input').on('change', function () {
+    var q9 = $('input[name=question9Radios]:checked', '#q-9').val();
+    console.log("q9 value= " + q9);
+
+    if (q9 === "option9d") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
+});
+
+$('#q-10 input').on('change', function () {
+    var q10 = $('input[name=question10Radios]:checked', '#q-10').val();
+    console.log("q10 value= " + q10);
+
+    if (q10 === "option10b") {
+        correct++;
+        console.log("# correct " + correct);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    } else {
+        incorrect++;
+        console.log("# incorrect " + incorrect);
+        $("#correct").html(correct);
+        $("#incorrect").html(incorrect);
+        unanswered--;
+        $("#unanswered").html(unanswered);
+    }
 });
 
 if ("#q-1 input" !== "question1a" || "question1b" || "question1c" || "question1d") {
@@ -318,7 +315,7 @@ if ("#q-1 input" !== "question1a" || "question1b" || "question1c" || "question1d
     $("#unanswered").html(unanswered);
     $("#correct").html(correct);
     $("#incorrect").html(incorrect);
-} 
+}
 
 if ("#q-2 input" === "question2a" || "question2b" || "question2c" || "question2d") {
     unanswered++;
@@ -397,7 +394,7 @@ function reset() {
     correct = 0;
     incorrect = 0;
     unanswered = 10;
-    
+
     $("#questions").hide();
     $("#user-report").hide();
     $("#instructions").show();
